235e2a8bb45cab5a3de02d066eb5ac8176c1ef67
Beau Charman <beau.charman@gmail.com>
2013-02-07 10:04:36 +1100

    Initaial Commit

---
+++ header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<!--[if IEMobile 7]>   <html class="no-js ie-mobile-7"> <![endif]-->
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" <?php language_attributes(); ?>> <!--<![endif]-->
+
+<head>
+
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <meta name="description" content="">
+  <meta name="viewport" content="width=device-width">
+
+  <title></title>
+
+  <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>" type="text/css" media="all" />
+  <link rel="pingback" href="<?php bloginfo( "pingback_url" ); ?>" />
+
+  <!--[if IE]>
+  <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+  <![endif]-->
+
+  <?php wp_head(); ?>
+
+</head>
+
+<body <?php body_class(); ?>>
---
+++ readme.md
@@ -0,0 +1,1 @@
+# A tiny, blank, WordPress theme.
