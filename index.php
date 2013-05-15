7237d1544a230d0a2e539e8cfd7b41fc798a3b49
Beau Charman <beau.charman@gmail.com>
2013-05-15 23:59:40 +1000

    added gitignore

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php if ( have_posts() ) : ?>
   <?php while ( have_posts() ) : the_post(); ?>
 
+    <h2><?php the_title(); ?></h2>
+    <?php the_contact(); ?>
+
   <?php endwhile; ?>
 <?php endif; ?>
